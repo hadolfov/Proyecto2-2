43bb5dd4b9428742eca51f11551631c43e5d028b
hadolfov <55640049+hadolfov@users.noreply.github.com>
2019-12-19 12:36:48 -0600

    cinco

---
+++ src/proyecto22/Lista.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto22;
+
+/**
+ *
+ * @author hadol
+ */
+public class Lista {
+    
+}
