79c8c9055f6f60eb7c453d1f52299d7885f7afdf
hadolfov <55640049+hadolfov@users.noreply.github.com>
2019-12-19 14:11:46 -0600

    seis

--- src/proyecto22/Lista.java
+++ src/proyecto22/Lista.java
@@ -5,10 +5,35 @@
  */
 package proyecto22;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author hadol
  */
 public class Lista {
+    ArrayList<Persona> lista = new ArrayList<>();
     
+    public void agregar(Persona p){
+        this.lista.add(p);
+    }
+    
+    public Persona buscarNombre(String nomb){
+
+        for (int i=0; i<this.lista.size(); i++) 
+        { 
+            if (this.lista.get(i).getNombre().equals(nomb)){
+                if (this.lista.get(i).getClass().equals(Estudiante.class)){
+                    Estudiante p = new Estudiante(this.lista.get(i).getNombre(), this.lista.get(i).getApellido(), this.lista.get(i).getEdad(), this.lista.get(i).getSexo(), this.lista.get(i).);
+                } else {
+                    
+                }
+                
+                
+            } else {
+                //Persona p = new Persona("-", "-", "-", "-");
+            }
+        }
+        return p;
+    }
 }
