a69b73e37c541362ccc3f263dbd04aa367b89923
hadolfov <55640049+hadolfov@users.noreply.github.com>
2019-12-19 12:37:02 -0600

    cinco

--- src/proyecto22/Estudiante.java
+++ src/proyecto22/Estudiante.java
@@ -10,6 +10,11 @@ package proyecto22;
  * @author Laboratorio CISCO
  */
 public class Estudiante extends Persona{
-    private String nota;
+    private final String nota;
+
+    public Estudiante(String nombre, String apellido, String edad, String sexo,String nota) {
+        super(nombre, apellido, edad, sexo);
+        this.nota = nota;
+    }
     
 }
