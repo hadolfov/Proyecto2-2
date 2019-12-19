17118971c8849d18440b2108a01ac40fed4ae460
hadolfov <55640049+hadolfov@users.noreply.github.com>
2019-12-19 12:36:56 -0600

    cinco

--- src/proyecto22/Profesor.java
+++ src/proyecto22/Profesor.java
@@ -9,7 +9,21 @@ package proyecto22;
  *
  * @author Laboratorio CISCO
  */
+
+
 public class Profesor extends Persona{
     private String horario;
-    
+
+    public Profesor(String nombre, String apellido, String edad, String sexo, String horario) {
+        super(nombre, apellido, edad, sexo);
+        this.horario = horario;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
 }
